d63ffea8c7fc7de8db498448a8ead63ba2cb42ad
Justin Lang <91992668+jlang61@users.noreply.github.com>
2024-12-05 16:17:28 -0800

    adding imaging for trees

--- x/merkledb/raw_disk.go
+++ x/merkledb/raw_disk.go
@@ -9,12 +9,11 @@ import (
 	"errors"
 	"fmt"
 	"log"
-	// "strconv"
-
-	// "log"
-
-	// "log"
+	"math"
+	"os"
 	"sort"
+
+	"gopkg.in/dnaeon/go-binarytree.v1"
 
 	"github.com/ava-labs/avalanchego/utils/maybe"
 )
@@ -100,63 +99,148 @@ func (r *rawDisk) closeWithRoot(root maybe.Maybe[*node]) error {
 }
 
 func (r *rawDisk) getRootKey() ([]byte, error) {
-	return nil, errors.New("not implemented")
+	initialDiskAddr := diskAddress{
+		offset: 17,
+		size:   16,
+	}
+	rootKeyDiskAddrBytes, _ := r.dm.get(initialDiskAddr)
+	rootKeyDiskAddr := diskAddress{}
+	rootKeyDiskAddr.decode(rootKeyDiskAddrBytes)
+	rootKeyBytes, err := r.dm.get(rootKeyDiskAddr)
+	if err != nil {
+		return nil, err
+	}
+	return rootKeyBytes, nil
+}
+func byteToInt(b []byte) int {
+	i := 0
+	c := len(b) - 1
+	for _, b := range b {
+		i += int(b) * int(math.Pow(10, float64(c)))
+		c--
+	}
+	return i 
+}
+
+func insertToTree(root *binarytree.Node[int], parentKeyBytes []byte, childKeyBytes []byte) error {
+	keyInt := byteToInt(parentKeyBytes)
+	goodPredicate := func(n *binarytree.Node[int]) bool {
+		return n.Value == keyInt
+	}
+	var parent *binarytree.Node[int]
+	var ok bool
+
+	parent, ok = root.FindNode(goodPredicate)
+	if !ok {
+		return errors.New("parent not found")
+	}
+	childKeyInt := byteToInt(childKeyBytes)
+	if parent.Left == nil {
+		parent.Left = binarytree.NewNode(childKeyInt)
+	} else {
+		parent.Right = binarytree.NewNode(childKeyInt)
+	}
+	return nil 
 }
 
 func (r *rawDisk) printTree(rootDiskAddr diskAddress, changes *changeSummary) error {
-	// Iterate through the tree and print out the keys and disk addresses
+	// Initialize a slice to keep track of nodes that need to be processed
 	var remainingNodes []diskAddressWithKey
+
+	// Retrieve the root node from disk using its address
 	newRootNodeBytes, _ := r.dm.get(rootDiskAddr)
 	newRootNode := &dbNode{}
 	decodeDBNode_disk(newRootNodeBytes, newRootNode)
-	log.Printf("Root node %v with key {%v, %s}", rootDiskAddr, changes.rootChange.after.Value().key.length, changes.rootChange.after.Value().key.value)
-	parentKey := changes.rootChange.after.Value().key
-	for token, child := range newRootNode.children {
-    totalKeyBytes := append(parentKey.Bytes(), token)
-    totalKeyBytes = append(totalKeyBytes, child.compressedKey.Bytes()...)
-    totalKey := ToKey(totalKeyBytes)
 
-		// add the child diskaddr to the array remainingNodes
+	// Get the root key from the changes
+	rootKey := changes.rootChange.after.Value().key
+	log.Println(changes.rootChange.after.Value().key)
+
+	// Retrieve the root key bytes from disk
+	rootKeyBytes, err := r.getRootKey()
+	if err != nil {
+		return err
+	}
+
+	// Convert the root key bytes to an integer for the binary tree node
+	keyInt := byteToInt(rootKeyBytes)
+
+	// Create the root node of the binary tree for visualization
+	root := binarytree.NewNode(keyInt)
+
+	// Iterate over the children of the root node
+	for token, child := range newRootNode.children {
+		// Construct the total key by appending the token and compressed key
+		totalKeyBytes := append(rootKey.Bytes(), token)
+		totalKeyBytes = append(totalKeyBytes, child.compressedKey.Bytes()...)
+		totalKey := ToKey(totalKeyBytes)
+
+		 // Add the child disk address and key to the list of remaining nodes
 		diskAddressKey := diskAddressWithKey{addr: &child.diskAddr, key: totalKey}
+
+		// Insert the child node into the binary tree
+		err := insertToTree(root, rootKeyBytes, totalKeyBytes)
+		if err != nil {
+			return err
+		}
+
 		remainingNodes = append(remainingNodes, diskAddressKey)
 
-    log.Printf("Token of %v with child compressed key %v", token, child.compressedKey)
-		log.Printf("Child with key %v with parent key %v", totalKey, parentKey)
+		// Log information about the child node
+		log.Printf("Token of %v with child compressed key %v", token, child.compressedKey)
+		log.Printf("Child with key %v with parent key %v", totalKey, rootKey)
 	}
+
+	// Iterate over the remaining nodes to process the entire tree
 	for _, diskAddressKey := range remainingNodes {
-		// iterate through the first instance of the remainingNodes
-		// and print out the key and disk address
+		// Retrieve the parent key and disk address
 		diskAddress := diskAddressKey.addr
 		parentKey := diskAddressKey.key
+
+		// Read the child node data from disk
 		childBytes, err := r.dm.get(*diskAddress)
 		if err != nil {
 			return err
 		}
 		childNode := &dbNode{}
 		decodeDBNode_disk(childBytes, childNode)
-		for token, child := range childNode.children {
-      totalKeyBytes := append(parentKey.Bytes(), token)
-      totalKeyBytes = append(totalKeyBytes, child.compressedKey.Bytes()...)
-      totalKey := ToKey(totalKeyBytes)
 
+		// Iterate over the children of the current node
+		for token, child := range childNode.children {
+			// Construct the total key for the child node
+			totalKeyBytes := append(parentKey.Bytes(), token)
+			totalKeyBytes = append(totalKeyBytes, child.compressedKey.Bytes()...)
+			totalKey := ToKey(totalKeyBytes)
+
+			// Insert the child node into the binary tree
+			err := insertToTree(root, parentKey.Bytes(), totalKeyBytes)
+			if err != nil {
+				return err
+			}
+
+			// Add the child node to the list of remaining nodes
 			diskAddressKey := diskAddressWithKey{addr: &child.diskAddr, key: totalKey}
 			remainingNodes = append(remainingNodes, diskAddressKey)
 
+			// Log information about the child node
 			log.Printf("Child with key %v with parent key %v", totalKey, parentKey)
 		}
-		// remove the node from remainingNodes array
+
+		// Remove the processed node from the list
 		remainingNodes = remainingNodes[1:]
-
 	}
+
+	// Write the binary tree structure to a DOT file for visualization
+	file, err := os.Create("tree.dot")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Output the binary tree in DOT format
+	root.WriteDot(file)
 	return nil
-
 }
-
-// type assertion to ensure that
-// pointer to rawdisk implements disk interface
-//var _ Disk = &rawDisk{}
-
-// return new error for iterator
 
 func (r *rawDisk) writeChanges(ctx context.Context, changes *changeSummary) error {
 	var keys []Key
@@ -189,9 +273,9 @@ func (r *rawDisk) writeChanges(ctx context.Context, changes *changeSummary) erro
 		// Iterate through remainingNodes in the node
 		for token, child := range nodeChange.after.children {
 			// Create the complete key (current key + compressed key of the child)
-      completeKeyBytes := append(k.Bytes(), token)
-      completeKeyBytes = append(completeKeyBytes, child.compressedKey.Bytes()...)
-      completeKey := ToKey(completeKeyBytes)
+			completeKeyBytes := append(k.Bytes(), token)
+			completeKeyBytes = append(completeKeyBytes, child.compressedKey.Bytes()...)
+			completeKey := ToKey(completeKeyBytes)
 			log.Printf("Creating child's completekey %v for parent %v", completeKey, k)
 			// Check whether or not there exists a value for the child in the map
 			if childrenNodes[completeKey] != (diskAddress{}) {
@@ -226,9 +310,9 @@ func (r *rawDisk) writeChanges(ctx context.Context, changes *changeSummary) erro
 			// tokenSize := len(string(token))
 			// // Create the complete key (current key + compressed key of the child)
 			// completeKey := Key{length: k.length + tokenSize + child.compressedKey.length, value: k.value + strconv.Itoa(tokenInt) +child.compressedKey.value}
-      completeKeyBytes := append(k.Bytes(), token)
-      completeKeyBytes = append(completeKeyBytes, child.compressedKey.Bytes()...)
-      completeKey := ToKey(completeKeyBytes)
+			completeKeyBytes := append(k.Bytes(), token)
+			completeKeyBytes = append(completeKeyBytes, child.compressedKey.Bytes()...)
+			completeKey := ToKey(completeKeyBytes)
 			log.Printf("Creating root node's child's completekey %v for parent %v", completeKey, k)
 			// Check whether or not there exists a value for the child in the map
 			if childrenNodes[completeKey] != (diskAddress{}) {
@@ -332,11 +416,11 @@ func (r *rawDisk) getNode(key Key, hasValue bool) (*node, error) {
 		return nil, err
 	}
 
-  // log.Printf("Here")
+	// log.Printf("Here")
 	currKeyString := string(rootKeyBytes[:])
-	currKey := Key{length: len(currKeyString)*8, value: currKeyString}
-  // currKey, err := decodeKey(rootKeyBytes[:])
-  // log.Printf(currKey.value)
+	currKey := Key{length: len(currKeyString) * 8, value: currKeyString}
+	// currKey, err := decodeKey(rootKeyBytes[:])
+	// log.Printf(currKey.value)
 	if err != nil {
 		return nil, err
 	}
@@ -345,26 +429,26 @@ func (r *rawDisk) getNode(key Key, hasValue bool) (*node, error) {
 		return nil, errors.New("Key doesn't match rootkey")
 	}
 
-	keylen := currKey.length// keeps track of where to start comparing prefixes in the key i.e. the length of key iterated so far
-  // log.Printf("Key length %d", currKey.length)
-  // log.Printf("Key length %d", keylen)
-  // log.Printf("Key length %d", key.length)
+	keylen := currKey.length // keeps track of where to start comparing prefixes in the key i.e. the length of key iterated so far
+	// log.Printf("Key length %d", currKey.length)
+	// log.Printf("Key length %d", keylen)
+	// log.Printf("Key length %d", key.length)
 
 	// while the entire path hasn't been matched
 	for keylen < (key.length) {
 		// confirm that a child exists and grab its address before attempting to load it
-    log.Printf("Token: %v",key.Token(keylen, tokenSize))
-    log.Printf("currentDbNode value %s", currentDbNode.value.Value())
-    log.Printf("num of children %d", len(currentDbNode.children))
+		log.Printf("Token: %v", key.Token(keylen, tokenSize))
+		log.Printf("currentDbNode value %s", currentDbNode.value.Value())
+		log.Printf("num of children %d", len(currentDbNode.children))
 		for token, child := range currentDbNode.children {
-      log.Printf("Token: %v for Child: %s", (token), child.compressedKey.value)
-    }
+			log.Printf("Token: %v for Child: %s", (token), child.compressedKey.value)
+		}
 		nextChildEntry, hasChild := currentDbNode.children[key.Token(keylen, tokenSize)]
 
-    keylen += tokenSize
-    if !hasChild {
+		keylen += tokenSize
+		if !hasChild {
 			return nil, errors.New("Key not found in node's children")
-    }
+		}
 
 		if !key.iteratedHasPrefix(nextChildEntry.compressedKey, keylen, tokenSize) {
 			// there was no child along the path or the child that was there doesn't match the remaining path

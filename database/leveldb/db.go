8bbcdb7f68553616e4df8afaf3f73cf187f881d0
Justin Lang <jlang61@ucsb.edu>
2025-03-13 14:01:50 -0700

    adding 12 mb comaprison with leveldb being 12 mb

--- database/leveldb/db.go
+++ database/leveldb/db.go
@@ -191,7 +191,7 @@ func New(file string, configBytes []byte, log logging.Logger, reg prometheus.Reg
 		BlockCacheCapacity:     DefaultBlockCacheSize,
 		DisableSeeksCompaction: true,
 		OpenFilesCacheCapacity: DefaultHandleCap,
-		WriteBuffer:            DefaultWriteBufferSize / 2,
+		WriteBuffer:            DefaultWriteBufferSize,
 		FilterBitsPerKey:       DefaultBitsPerKey,
 		MaxManifestFileSize:    DefaultMaxManifestFileSize,
 		MetricUpdateFrequency:  DefaultMetricUpdateFrequency,
